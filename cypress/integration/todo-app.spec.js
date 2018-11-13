9281d383506ef93806bbfb3df7c05d6df60e535a
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-12 20:23:51 -0800

    added test for searching todos

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -4,4 +4,8 @@ describe('Test Todo-It App', () => {
     cy.get('#todo-text .input').type('test todo {enter}');
     cy.get('#todo-text .input').type('test1 todo {enter}');
   });
+  it('should search for todo', () => {
+    cy.get('#search-text').type('test1 {enter}');
+    cy.get('.list-title').contains('You have 1 todo left');
+  });
 });
