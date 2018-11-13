bc2ded905cfef9e123bd47fdc5a61cf7a6658bee
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-12 20:09:53 -0800

    added cypress tests to project, along with cypress dir

---
+++ cypress/integration/todo-app.spec.js
@@ -0,0 +1,6 @@
+describe("Test Todo-It App", () => {
+  it("Should create a todo", () => {
+    cy.visit("https://attractive-cobweb.surge.sh/");
+    cy.get("#todo-text .input").type("test todo {enter}");
+    cy.get("#todo-text .input").type("test1 todo {enter}");
+  });
