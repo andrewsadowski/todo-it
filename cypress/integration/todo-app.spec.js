f68621481ea081d507bb9805070d76c579df9643
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-12 20:11:07 -0800

    fixed eslint errors with .spec.js

--- cypress/integration/todo-app.spec.js
+++ cypress/integration/todo-app.spec.js
@@ -1,6 +1,7 @@
-describe("Test Todo-It App", () => {
-  it("Should create a todo", () => {
-    cy.visit("https://attractive-cobweb.surge.sh/");
-    cy.get("#todo-text .input").type("test todo {enter}");
-    cy.get("#todo-text .input").type("test1 todo {enter}");
+describe('Test Todo-It App', () => {
+  it('Should create a todo', () => {
+    cy.visit('https://attractive-cobweb.surge.sh/');
+    cy.get('#todo-text .input').type('test todo {enter}');
+    cy.get('#todo-text .input').type('test1 todo {enter}');
   });
+});
